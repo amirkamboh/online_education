59ce2c8fc8db554254808c66c4ac7cacd95b2326
amirkamboh <amirkamboh036@gmail.com>
2024-01-20 23:15:05 +0500

    Learning Experience

--- src/pages/hompage/Components/learnsection.js
+++ src/pages/hompage/Components/learnsection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import LearnImg from '../../../media/Images/Casual.png'
+import LearnImg from '../../../media/Images/workingwomen.PNG'
 
 const Learnsection = () => {
   return (
@@ -10,7 +10,29 @@ const Learnsection = () => {
                 <div className="learning_experience_img">
                   <img src={LearnImg} alt="" />
                 </div>
-                <div className="learning_experience_content">B</div>
+                <div className="learning_experience_content">
+                  <h2>Premium <span>Learning</span> </h2> <h3>Experience</h3>
+            
+                <div className='learning_experience_content_divid'>
+            
+                    <div className="learning_experience_smallcard">
+                    <div className="learning_experience_smallcard_layout"></div>
+                    </div>
+                    <div className="learning_experience_smallhead">
+                      <h3>Easily Accessible</h3>
+                      <p>Learning Will fell Very Comfortable With Courslab.</p>
+                    </div>
+
+                    <div className="learning_experience_smallcard">
+                    <div className="learning_experience_smallcard_layout"></div>
+                    </div>
+                    <div className="learning_experience_smallhead">
+                      <h3>Fun Learning Expe</h3>
+                      <p>Learning Will fell Very Comfortable With Courslab.</p>
+                    </div>
+
+                  </div>
+                </div>
             </div>
          </div>
        </div>

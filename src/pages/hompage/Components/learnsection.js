2dc4db36c28fa07ad7d491f95799d43c884f8501
amirkamboh <amirkamboh036@gmail.com>
2024-01-20 18:53:04 +0500

    learn

--- src/pages/hompage/Components/learnsection.js
+++ src/pages/hompage/Components/learnsection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Learn_Img from '../../../media/Images/Logo_Education.png'
 
 const Learnsection = () => {
   return (
@@ -6,7 +7,10 @@ const Learnsection = () => {
        <div className="learning_experience_section">
          <div className="learning_experience_width">
             <div className="leaning_experience_divid">
-                
+                <div className="learning_experience_img">
+                  <img src={Learn_Img} alt="" />
+                </div>
+                <div className="learning_experience_content">B</div>
             </div>
          </div>
        </div>
